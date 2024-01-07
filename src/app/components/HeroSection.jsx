cbf245f424fce2509cbd8e53fb8db7c1540649b3
dmehta3 <dakshkmehta@gmail.com>
2024-01-06 22:11:40 -0500

    header done

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,23 +1,13 @@
+"use client";
 import React from 'react'
 import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
   return (
     <section>
         <div className ="grid grid-cols-1 sm:grid-cols-12">
-            <div className="col-span-7 place-self-center text-center sm:text-left">
-              <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                Hi, I'm Daksh!
-              </h1>
-              <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-                I'm an aspiring Software Developer looking for interships in Summer 2024
-              </p>
-              <div>
-                <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-3 bg-gradient-to-br from-red-500 via-orange-500 to-yellow-500 hover:bg-slate-400 text-white ">Hire Me</button>
-                <button className="px-6 py-3 w-full sm:w-fit rounded-full  bg-transparent hover:bg-slate-800 text-white border border-white mt-3">Resume</button>
-              </div>
-            </div>
-            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          {/* <div className="col-span-5 place-self-center mt-4 lg:mt-0">
               <div className="rounded-full bg-[#28282B] w-[250px] h-[250px] lg:w-[350px] lg:h-[350px] relative">
                 <Image
                 src="/images/pfp.png"
@@ -25,6 +15,46 @@ const HeroSection = () => {
                 className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                 width = {200}
                 height = {200}
+                />
+              </div>
+            </div> */}
+            <div className="col-span-7 place-self-center text-center sm:text-left">
+              <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                Hi, I'm
+                <br></br>
+                <TypeAnimation
+                  sequence={[
+                    // Same substring at the start will only be typed out once, initially
+                    ' Daksh!',
+                    1000, // wait 1s before replacing "Mice" with "Hamsters"
+                    ' an aspiring Software Engineer',
+                    1000,
+                  ]}
+                  wrapper="span"
+                  speed={40}
+                  repeat={Infinity}
+                />
+              </h1>
+              <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+                Currently seeking internships for Summer 2024
+              </p>
+              <div>
+                <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-3 bg-gradient-to-br from-red-500 via-orange-500 to-yellow-500 hover:bg-slate-400 text-white ">
+                  Hire Me
+                </button>
+                <button className="px-1 py-1 w-full sm:w-fit rounded-full  bg-gradient-to-br from-red-500 via-orange-500 to-yellow-500 hover:bg-slate-800 text-white mt-3">
+                  <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Resume</span>
+                </button>
+              </div>
+            </div>
+            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+              <div className="rounded-full bg-[#28282B] w-[250px] h-[250px] lg:w-[300px] lg:h-[300px] relative">
+                <Image
+                src="/images/pfp.png"
+                alt="Professional Headshot"
+                className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                width = {250}
+                height = {250}
                 />
               </div>
             </div>

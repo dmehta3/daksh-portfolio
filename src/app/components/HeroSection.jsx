2b7edc85272df5d9f1ef5a15f392c669f2e5bd94
dmehta3 <dakshkmehta@gmail.com>
2024-01-06 22:43:38 -0500

    starting nav bar

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,28 +7,16 @@ const HeroSection = () => {
   return (
     <section>
         <div className ="grid grid-cols-1 sm:grid-cols-12">
-          {/* <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-              <div className="rounded-full bg-[#28282B] w-[250px] h-[250px] lg:w-[350px] lg:h-[350px] relative">
-                <Image
-                src="/images/pfp.png"
-                alt="Professional Headshot"
-                className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                width = {200}
-                height = {200}
-                />
-              </div>
-            </div> */}
-            <div className="col-span-7 place-self-center text-center sm:text-left">
+            <div className="col-span-7 text-center sm:text-left">
               <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
                 Hi, I'm
                 <br></br>
                 <TypeAnimation
                   sequence={[
-                    // Same substring at the start will only be typed out once, initially
                     ' Daksh!',
-                    1000, // wait 1s before replacing "Mice" with "Hamsters"
+                    1500,
                     ' an aspiring Software Engineer',
-                    1000,
+                    1500
                   ]}
                   wrapper="span"
                   speed={40}
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import HeroSection from './components/HeroSection'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] container-fluid">
-     <div class="container mx-auto px-12 py-4">
+     <div class="container mx-auto px-8 py-8">
        <HeroSection />
      </div>
     </main>
